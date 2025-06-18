b993a68292098c24d48fa5474e98b535dcbf85fa
Minh Hieu <minhhieudoqb255@gmail.com>
2025-06-18 14:53:19 +0700

    Add images to cloudiary

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,37 +1,42 @@
 const express = require("express");
-const multer  = require('multer');
+const multer = require("multer");
 const route = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+// const storageMulter = require("../../helpers/storageMulter");
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 
 const validate = require("../../validates/admin/product.validate");
 
-route.get('/',controller.index);
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares.js");
 
-route.patch('/change-status/:status/:id',controller.changeStatus);
+route.get("/", controller.index);
 
-route.patch('/change-multi',controller.changeMulti);
+route.patch("/change-status/:status/:id", controller.changeStatus);
 
-route.delete('/delete/:id',controller.deleteItem);
+route.patch("/change-multi", controller.changeMulti);
 
-route.get('/create',controller.create);
+route.delete("/delete/:id", controller.deleteItem);
+
+route.get("/create", controller.create);
 
 route.post(
-    '/create',
-    upload.single('thumbnail'),
-    validate.createPost,
-    controller.createPost);
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
+);
 
-route.get('/edit/:id',controller.edit);
+route.get("/edit/:id", controller.edit);
 
 route.patch(
-    '/edit/:id',
-    upload.single('thumbnail'),
-    validate.createPost,
-    controller.editPatch);
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.editPatch
+);
 
-route.get('/detail/:id',controller.detail);
+route.get("/detail/:id", controller.detail);
 
-module.exports = route; 
+module.exports = route;
